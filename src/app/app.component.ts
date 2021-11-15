4af9b59518df57c18126007492965f7a73c14272
aboubakar sokoba <sokobachristian@yahoo.fr>
2021-11-16 00:52:02 +0100

    setting up the project and git repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app works!';
+  total: number;
+  showCounter: boolean = true;
+  constructor() {
+    // dependencies injection
+  }
+  ngOnInit() {
+    this.total = 0;
+    this.showCounter = true;
+  }
+  public increment() {
+    this.total++;
+  }
+
+  public changeCounterDisplay() {
+    this.showCounter = !this.showCounter;
+  }
 }
